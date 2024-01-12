a4f971497b2f8eda1668c1252b5311c2c1507692
ehsan1731 <ehsans2vt@gmail.com>
2024-01-12 19:21:05 +0330

    7 ezafe shod

---
+++ 7/Guardian.cs
@@ -0,0 +1,31 @@
+﻿public interface IPerson
+{
+    string Name { get; set; }
+    string CodeMeli { get; set; }
+    void Enter();
+}
+
+
+public class People : IPerson
+{
+    public string Name { get; set; }
+    public string CodeMeli { get; set; }
+
+    public void Enter()
+    {
+        Console.WriteLine($"{Name} is in the public hall.");
+    }
+}
+
+
+public class Employee : IPerson
+{
+    public string Name { get; set; }
+    public string CodeMeli { get; set; }
+    public string Id { get; set; }
+
+    public void Enter()
+    {
+        Console.WriteLine($"{Name} is in the company.");
+    }
+}
---
+++ 7/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+var people = new List<IPerson>();
+
+
+people.Add(new People { Name = "akbar", CodeMeli = "3384569987" });
+people.Add(new People { Name = "asghar", CodeMeli = "2125698634" });
+
+
+people.Add(new Employee { Name = "ehsan", CodeMeli = "3380435295", Id = "55g55" });
+people.Add(new Employee { Name = "ali", CodeMeli = "2125687416", Id = "12f56" });
+
+
+foreach (var person in people)
+{
+    person.Enter();
+}
